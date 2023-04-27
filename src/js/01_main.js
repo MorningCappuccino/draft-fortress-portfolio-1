60aa215973387835af4c34378c084a12263926c7
Olga-Zhuleva <zhuleva_o@list.ru>
2023-04-27 16:04:57 +0300

    add swiper, photo teams

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -1,1 +1,15 @@
+
+'use strict'
+
 console.log('Hello console!')
+
+$(document).ready(function() {
+    $('.ourSkills__slider').slick({
+        arrows: true,
+        dots: false,
+        adaptiveHight: true,
+        slidesToShow: 4,
+
+    });
+});
+
