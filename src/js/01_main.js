53e63da855b2f2da0b14b3b03542dce7d3aff27e
Olga-Zhuleva <zhuleva_o@list.ru>
2023-04-28 17:32:21 +0300

    adaptive for slider

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -9,6 +9,27 @@ $(document).ready(function() {
         dots: false,
         adaptiveHight: true,
         slidesToShow: 4,
+        infinite: false,
+        swipe: true,
+        responsive:[
+            {
+                breakpoint: 1000,
+                    settings: {
+                    slidesToShow: 3
+                }
+
+            }, {
+                    breakpoint: 768,
+                    settings: {
+                    slidesToShow: 2
+                }
+            }, {
+                    breakpoint: 480,
+                    settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
 
     });
 });
