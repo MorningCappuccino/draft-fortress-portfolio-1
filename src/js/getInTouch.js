a6a3c10bd73266d2886b500e76dc17a902788a50
MorningCappuccino <knockknockwakeup@gmail.com>
2023-06-16 10:03:53 +0400

    fix: form preventDefault

--- src/js/getInTouch.js
+++ src/js/getInTouch.js
@@ -2,10 +2,12 @@
     const form = document.getElementById('getInTouchForm');
     const sendButton = document.querySelector('.mySkills__button');
 
-    sendButton.addEventListener('click', sendForm);
+    sendButton.addEventListener('click', (ev) => sendForm(ev));
 
     //TODO: form validation
-    function sendForm() {
+    function sendForm(ev) {
+        console.log("ev" + ev.preventDefault)
+        ev.preventDefault();
         // the file send_email.php should be placed in the root of server
         fetch('/send_email.php', {
             method: 'POST',
