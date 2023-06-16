73f7acd68594336a6bfd101e5ceec3d14954543a
MorningCappuccino <knockknockwakeup@gmail.com>
2023-06-16 10:42:19 +0400

    redirect to /build folder

--- src/send_email.php
+++ src/send_email.php
@@ -7,14 +7,14 @@
     $phone_number = $_POST['phone_number'];
 
     $subject = "Get in touch";
-    $subject2 = "Copy of your get in touch form";
-    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+    // $subject2 = "Copy of your get in touch form";
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'] . "\n\n" . $_POST['phone_number'];
+    // $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
 
     $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
+    // $headers2 = "From:" . $to;
     mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    // mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
     // echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
 
 ?>
