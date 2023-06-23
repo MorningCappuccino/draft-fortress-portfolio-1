7687cb289f68f414de01ccadc904cf41f5bd7cab
Valery Chyzhykau <godox.vitebsk@gmail.com>
2023-06-23 23:31:24 +0300

    fix textarea

--- src/js/preloader.js
+++ src/js/preloader.js
@@ -1,14 +1,14 @@
-const codeGo = "{ Code_Go }".split("");
+const codeGo = '{ Code_Go }'.split('');
 
-const preloader = document.documentElement.querySelector(".Preloader");
-const domTextElement = document.documentElement.querySelector(".Preloader__text");
-const body = document.documentElement.querySelector("body");
+const preloader = document.documentElement.querySelector('.Preloader');
+const domTextElement = document.documentElement.querySelector('.Preloader__text');
+const body = document.documentElement.querySelector('body');
 
 let interval;
 let count = 0;
 
-body.style["padding-right"] = `${preloader.offsetWidth - body.clientWidth}px`;
-body.style.overflow = "hidden";
+body.style['padding-right'] = `${preloader.offsetWidth - body.clientWidth}px`;
+body.style.overflow = 'hidden';
 
 const printText = () => {
   interval = setInterval(() => {
@@ -25,14 +25,14 @@ const printText = () => {
 
 const closePreloader = () => {
   setTimeout(() => {
-    preloader.classList.add("Preloader--hidden");
+    preloader.classList.add('Preloader--hidden');
   }, 2000);
 
   setTimeout(() => {
     preloader.remove();
-    body.removeAttribute("style");
+    body.removeAttribute('style');
   }, 2450);
 };
 
-window.addEventListener("DOMContentLoaded", printText);
-window.addEventListener("load", closePreloader);
+window.addEventListener('DOMContentLoaded', printText);
+window.addEventListener('load', closePreloader);
